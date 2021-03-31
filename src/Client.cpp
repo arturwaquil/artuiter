e4ffefe90f843d9114ef30f983d7f1da9e267660
Artur <arturwaquil@gmail.com>
2021-03-31 20:44:01 -0300

    deal with empty command and EOF (on client side)

--- src/Client.cpp
+++ src/Client.cpp
@@ -20,15 +20,21 @@ int main(int argc, char *argv[])
 
     while(true)
     {
+        // Read message (command) from user. If empty, ignore. If EOF, exit.
         std::string message = ui.read();
+        if (std::cin.eof()) break;
+        if (message.empty()) continue;
+
+        // Send command to server
         pkt = create_packet(command, 2, 1234, message);
         comm_manager.write_pkt(pkt);
 
+        // Receive server's reply to the command
         comm_manager.read_pkt(&pkt);
         std::cout << pkt.payload << std::endl;
     }
 
-    std::cout << "Exiting..." << std::endl;
+    std::cout << std::endl << "Exiting..." << std::endl;
 
     return 0;
 }
