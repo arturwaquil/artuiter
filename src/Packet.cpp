8897460b370b4e0c75414d82341a48664d0d2cbe
Artur <arturwaquil@gmail.com>
2021-04-08 16:58:56 -0300

    empty packet payload before writing message to it

--- src/Packet.cpp
+++ src/Packet.cpp
@@ -10,6 +10,7 @@ packet create_packet(EventType type, uint16_t seqn, uint16_t timestamp, std::str
     pkt.type = type;
     pkt.seqn = seqn;
     pkt.timestamp = timestamp;
+    memset(pkt.payload, 0, MAX_MESSAGE_SIZE);
     strcpy(pkt.payload, payload.c_str());
     return pkt;
 }
